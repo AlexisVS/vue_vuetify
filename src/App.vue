e6ef604d2002883d55195cdbc74873e7c363b48f
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2021-12-19 22:27:13 +0100

    olk

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <template>
   <v-app>
-    <v-app-bar app class="blue">
-      <v-row>
-        <v-col align-self="center">
+    <v-app-bar app class="blue d-flex item-center">
+      <v-row cols="11">
           <v-app-bar-nav-icon class="white--text" @click="drawer = !drawer" />
-        </v-col>
-        <v-col align-self="center">
-          <v-switch></v-switch>
-        </v-col>
+      </v-row>
+      <v-row cols="1" align-self="end">
+          <v-switch v-model="$vuetify.theme.dark"></v-switch>
       </v-row>
     </v-app-bar>
     <v-navigation-drawer app v-model="drawer" class="blue lighten-4">
